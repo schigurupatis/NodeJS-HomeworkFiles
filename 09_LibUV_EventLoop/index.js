ae994d4b8999dbb5150f98debe67127b82cf5e6f
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-11-27 13:24:41 +0530

    working with file reading, synchronus & asynchronus way of execution of event loop and its behaviour

--- 09_LibUV_EventLoop/index.js
+++ 09_LibUV_EventLoop/index.js
@@ -1,5 +1,31 @@
+const fs = require('fs').promises;
+
 console.log("Starts")
 
+const a = 100;
 
+setImmediate(()=> console.log("setImmediate"));
+
+fs.readFile("./file.txt", "utf8", ()=> {
+    console.log("File Reading CB")
+});
+
+setTimeout(()=> console.log("Timer Expired"), 0);
+
+function printA() {
+    console.log("a=", a);
+}
+
+printA();
 
 console.log("Ends")
+
+
+/* 
+* Start
+* End
+* Timer Expired
+* File Reading CB
+* setImmediate
+* a=100
+*/
