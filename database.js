167d0d5a6944bac53dbee2f121611b39a3e34249
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-11-30 16:27:06 +0530

    MongoDB installation & created DB with user table in website

---
+++ database.js
@@ -0,0 +1,15 @@
+require("mongodb");
+
+const URI = "mongodb+srv://schigurupatis:FNwYaNkBi5jNF7oT@ssrenterprise.qlawi.mongodb.net/"
+
+
+
+
+// NOTES
+// Go to MongoDB Website
+// Create a free M0 Cluster 
+// Give the DataBase Name
+// Create a user
+// Get the connection String
+// Install MongoDB Compass
+// 
